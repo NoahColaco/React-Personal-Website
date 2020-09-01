5bc3b2cd981afe57effe4bc94bea15c2debfd904
Noah Colaco <noahcolaco@hotmail.com>
2020-09-01 17:45:31 -0400

    build fix

---
+++ build/precache-manifest.6fbffc15bb03dec4f964abd3afd2f595.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ede5dfd217631f8eba94b38030594254",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89944ed5ec68dc09adb0",
+    "url": "/static/css/main.57a85dac.chunk.css"
+  },
+  {
+    "revision": "9f310ec6bc4934bb6a40",
+    "url": "/static/js/2.3af04483.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3af04483.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89944ed5ec68dc09adb0",
+    "url": "/static/js/main.9713816b.chunk.js"
+  },
+  {
+    "revision": "a4a011ee4cff22dbe791",
+    "url": "/static/js/runtime-main.d32ca624.js"
+  },
+  {
+    "revision": "a76e2c215721f19da9401172ab9b3474",
+    "url": "/static/media/background.a76e2c21.jpg"
+  },
+  {
+    "revision": "968ea62882943e88bbd318ae5fa67429",
+    "url": "/static/media/linkedin.968ea628.png"
+  }
+]);

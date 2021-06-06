ab7b39c58a83450f94feec8e3bc50fbedfa65708
Noah Colaco <31391947+NoahColaco@users.noreply.github.com>
2021-06-06 12:14:04 -0400

    Update AboutMe.js

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -16,7 +16,7 @@ class AboutMe extends Component{
                 <div class="container">
                     <section id="main">
                         <h1>Welcome</h1>
-                        <p>Hi! My name is Noah Colaco and I am a fourth-year Computer Science student at Ryerson University and I am currently on a co-op term with Ontario Treasury Board Secretariat. I am seeking a software-related position and I am available to start September 2021. Feel free to explore my personal projects and skills, as well as contact me at the bottom of the page through my LinkedIn or Email!</p>
+                        <p>Hi! My name is Noah Colaco and I am a fourth-year Computer Science student at Ryerson University and I am currently on a co-op term with Ontario Treasury Board Secretariat. I am seeking a software-related position and I am available to start January 2022. Feel free to explore my personal projects and skills, as well as contact me at the bottom of the page through my LinkedIn or Email!</p>
                     </section>
                 </div>
                 
